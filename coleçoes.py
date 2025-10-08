cfd7ae2d07f66826a91eaba497661877e67d8ba1
juliavazz99 <juliamoreira@colegiopaulodetarso.com.br>
2025-10-08 16:08:47 +0000

    f

---
+++ coleçoes.py
@@ -0,0 +1,7 @@
+numeros = [12, 7, 25, 3, 18]
+
+print("Maior valor:", max(numeros))
+print("Menor valor:", min(numeros))
+print("Lista ordenada:", sorted(numeros))
+print("Quantidade de elementos:", len(numeros))
+print("Média dos valores:", sum(numeros) / len(numeros))
